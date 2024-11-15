7ee267491df432fd7b1fd175bb8c3a3943af7d6e
fadhilelrizanda <fadhilelrizanda@gmail.com>
2024-11-16 05:21:37 +0700

    add

--- 3.inference_dynamic.py
+++ 3.inference_dynamic.py
@@ -74,7 +74,7 @@ def infer_video(engine_file_path, input_video, output_video, batch_size, labels)
     fps = int(cap.get(cv2.CAP_PROP_FPS))
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
     out = cv2.VideoWriter(output_video, fourcc, fps, (width, height))
     print(fps)
 
